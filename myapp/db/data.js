7b1fe8a0abbf1804a020f6c9d9632f7932d2df28
teodelinalm <tlagosmarmol@udesa.edu.ar>
2025-04-14 18:11:38 -0300

    detalles del css

--- myapp/db/data.js
+++ myapp/db/data.js
@@ -14,9 +14,9 @@ const data = {
             descripcion: "Potente laptop con procesador i7 y tarjeta gráfica RTX 3060.",
             imagen: "/images/products/05dhzg0lrtq4i0d3wxqyjjakwmbmzr331426.png.avif",
             comentarios: [
-                { usuario: "Ana", texto: "¡Me encantó esta laptop!", fotoPerfil: "" },
-                { usuario: "Carlos", texto: "Rinde muy bien para gaming.", fotoPerfil: "" },
-                { usuario: "Luisa", texto: "Buena calidad-precio.", fotoPerfil: "" }
+                { usuario: "Ana", texto: "¡Me encantó esta laptop!", fotoPerfil: "/images/users/fotoperfil.png" },
+                { usuario: "Carlos", texto: "Rinde muy bien para gaming.", fotoPerfil: "/images/users/fotoperfil.png" },
+                { usuario: "Luisa", texto: "Buena calidad-precio.", fotoPerfil: "/images/users/fotoperfil.png" }
             ]
         },
         {
@@ -25,9 +25,9 @@ const data = {
             descripcion: "Pantalla OLED, cámara de 108MP y batería de 5000mAh.",
             imagen: "/images/products/img-samsung-galaxy-s10.jpg",
             comentarios: [
-                { usuario: "Pedro", texto: "La cámara es increíble.", fotoPerfil: "" },
-                { usuario: "María", texto: "Me dura todo el día la batería.", fotoPerfil: "" },
-                { usuario: "Julián", texto: "Buena alternativa a los de gama alta.", fotoPerfil: "" }
+                { usuario: "Pedro", texto: "La cámara es increíble.", fotoPerfil: "/images/users/fotoperfil.png" },
+                { usuario: "María", texto: "Me dura todo el día la batería.", fotoPerfil: "/images/users/fotoperfil.png" },
+                { usuario: "Julián", texto: "Buena alternativa a los de gama alta.", fotoPerfil: "/images/users/fotoperfil.png" }
             ]
         },
         {
@@ -36,9 +36,9 @@ const data = {
             descripcion: "Cancelación de ruido y sonido envolvente.",
             imagen: "/images/products/auricularesinalambricos.webp",
             comentarios: [
-                { usuario: "Dani", texto: "Se escuchan genial.", fotoPerfil: "" },
-                { usuario: "Sofía", texto: "Son súper cómodos.", fotoPerfil: "" },
-                { usuario: "Lucas", texto: "Buena autonomía de batería.", fotoPerfil: "" }
+                { usuario: "Dani", texto: "Se escuchan genial.", fotoPerfil: "/images/users/fotoperfil.png" },
+                { usuario: "Sofía", texto: "Son súper cómodos.", fotoPerfil: "/images/users/fotoperfil.png" },
+                { usuario: "Lucas", texto: "Buena autonomía de batería.", fotoPerfil: "/images/users/fotoperfil.png" }
             ]
         },
         {
@@ -47,9 +47,9 @@ const data = {
             descripcion: "Auriculares inalambricos por bluetooth",
             imagen: "/images/products/airpods.jpg",
             comentarios: [
-                { usuario: "Teo", texto: "Muy comods", fotoPerfil: "" },
-                { usuario: "Cata", texto: "Faciles de transportar", fotoPerfil: "" },
-                { usuario: "Clemen", texto: "Muy lindos.", fotoPerfil: "" }
+                { usuario: "Teo", texto: "Muy comods", fotoPerfil: "/images/users/fotoperfil.png" },
+                { usuario: "Cata", texto: "Faciles de transportar", fotoPerfil: "/images/users/fotoperfil.png" },
+                { usuario: "Clemen", texto: "Muy lindos.", fotoPerfil: "/images/users/fotoperfil.png" }
             ]
         },
         {
@@ -58,9 +58,9 @@ const data = {
             descripcion: "Un reloj inteligente con monitoreo de actividad y notificaciones.",
             imagen: "/images/products/smartwatchgalaxy.webp",
             comentarios: [
-                { usuario: "Mar", texto: "Me encanta su diseño, aunque a veces se congela", fotoPerfil: "" },
-                { usuario: "Lucia", texto: "Es practico, pero la bateria no dura tanto", fotoPerfil: "" },
-                { usuario: "Valen", texto: "Es lindo y funcional, me re gusta", fotoPerfil: "" }
+                { usuario: "Mar", texto: "Me encanta su diseño, aunque a veces se congela", fotoPerfil: "/images/users/fotoperfil.png" },
+                { usuario: "Lucia", texto: "Es practico, pero la bateria no dura tanto", fotoPerfil: "/images/users/fotoperfil.png" },
+                { usuario: "Valen", texto: "Es lindo y funcional, me re gusta", fotoPerfil: "/images/users/fotoperfil.png" }
             ]
         },
         {
@@ -69,9 +69,9 @@ const data = {
             descripcion: "Tablet ligera y potente",
             imagen: "/images/products/ipadair.jpg",
             comentarios: [
-                { usuario: "Ana", texto: "La pantalla es nítida, me encanta", fotoPerfil: "" },
-                { usuario: "Cata", texto: "La bateria anda bien, pero se siente algo lenta en algunas apps", fotoPerfil: "" },
-                { usuario: "Lola", texto: "Es buena para la facu y para divertirte, me gusta", fotoPerfil: "" }
+                { usuario: "Ana", texto: "La pantalla es nítida, me encanta", fotoPerfil: "/images/users/fotoperfil.png" },
+                { usuario: "Cata", texto: "La bateria anda bien, pero se siente algo lenta en algunas apps", fotoPerfil: "/images/users/fotoperfil.png" },
+                { usuario: "Lola", texto: "Es buena para la facu y para divertirte, me gusta", fotoPerfil: "/images/users/fotoperfil.png" }
             ]
         },
         {
@@ -80,9 +80,9 @@ const data = {
             descripcion: "Auriculares con cancelación de ruido, ideales para escuchar música sin distracciones.",
             imagen: "/images/products/auricularesinalambricosSony.jpg",
             comentarios: [
-                { usuario: "Sofi", texto: "La calidad de sonido es increíble, aunque a veces se desconectan", fotoPerfil: "" },
-                { usuario: "Rosa", texto: "Muy cómodos", fotoPerfil: "" },
-                { usuario: "Ale", texto: "Buen producto", fotoPerfil: "" }
+                { usuario: "Sofi", texto: "La calidad de sonido es increíble, aunque a veces se desconectan", fotoPerfil: "/images/users/fotoperfil.png" },
+                { usuario: "Rosa", texto: "Muy cómodos", fotoPerfil: "/images/users/fotoperfil.png" },
+                { usuario: "Ale", texto: "Buen producto", fotoPerfil: "/images/users/fotoperfil.png" }
             ]
         },
         {
@@ -91,9 +91,9 @@ const data = {
             descripcion: "Un mouse personalizable para gamers, con alta precisión y botones programables.",
             imagen: "/images/products/mousegamer.jpg",
             comentarios: [
-                { usuario: "Juan", texto: "Lo uso y me encanta la respuesta", fotoPerfil: "" },
-                { usuario: "Mica", texto: "Buen mouse", fotoPerfil: "" },
-                { usuario: "Julieta", texto: "Es practico y se siente muy bien en la mano, lo recomiendo", fotoPerfil: "" }
+                { usuario: "Juan", texto: "Lo uso y me encanta la respuesta", fotoPerfil: "/images/users/fotoperfil.png" },
+                { usuario: "Mica", texto: "Buen mouse", fotoPerfil: "/images/users/fotoperfil.png" },
+                { usuario: "Julieta", texto: "Es practico y se siente muy bien en la mano, lo recomiendo", fotoPerfil: "/images/users/fotoperfil.png" }
             ]
         },
         {
@@ -102,9 +102,9 @@ const data = {
             descripcion: "Teclado con switches mecánicos veloces, excelente para escribir y jugar.",
             imagen: "/images/products/teclado.webp",
             comentarios: [
-                { usuario: "Paula", texto: "Me encanta el sonido de las teclas", fotoPerfil: "" },
-                { usuario: "Isa", texto: "Buen teclado, aunque a veces es medio ruidoso", fotoPerfil: "" },
-                { usuario: "Leo", texto: "super rápido y facil de usar, xD", fotoPerfil: "" }
+                { usuario: "Paula", texto: "Me encanta el sonido de las teclas", fotoPerfil: "/images/users/fotoperfil.png" },
+                { usuario: "Isa", texto: "Buen teclado, aunque a veces es medio ruidoso", fotoPerfil: "/images/users/fotoperfil.png" },
+                { usuario: "Leo", texto: "super rápido y facil de usar, xD", fotoPerfil: "/images/users/fotoperfil.png" }
             ]
         },
         {
@@ -113,9 +113,9 @@ const data = {
             descripcion: "Monitor ultra ancho ideal para multitarea y diseño.",
             imagen: "/images/products/medium01.jpg.avif",
             comentarios: [
-                { usuario: "Mateo", texto: "La calidad de imagen es buena, me encanta como amplía la pantalla", fotoPerfil: "" },
-                { usuario: "Tomas", texto: "Es genial para editar fotos y videos", fotoPerfil: "" },
-                { usuario: "Sol", texto: "Buen monitor, pero ojalá tuviera más puertos", fotoPerfil: "" }
+                { usuario: "Mateo", texto: "La calidad de imagen es buena, me encanta como amplía la pantalla", fotoPerfil: "/images/users/fotoperfil.png" },
+                { usuario: "Tomas", texto: "Es genial para editar fotos y videos", fotoPerfil: "/images/users/fotoperfil.png" },
+                { usuario: "Sol", texto: "Buen monitor, pero ojalá tuviera más puertos", fotoPerfil: "/images/users/fotoperfil.png" }
             ]
         }
     ]
